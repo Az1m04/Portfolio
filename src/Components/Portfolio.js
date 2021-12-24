c1f922c537cd23b12a2fcef9624258d82e3eccc0
Azim <azim.babu@simbaquartz.com>
2021-12-25 00:14:47 +0530

    'portfolio'

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Portfolio = ({ data }) => {
+  if (data) {
+    var projects = data.projects.map(function (projects) {
+      var projectImage = "images/portfolio/" + projects.image;
+      return (
+        <div key={projects.title}  className="columns portfolio-item" style={{width:'30%'}}>
+          <div className="item-wrap" style={{height:'250px',width:'250px',backgroundColor:'white'}}>
+            <a href={projects.url} title={projects.title}>
+              <img alt={projects.title} src={projectImage}  />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
+                </div>
+              </div>
+              <div className="link-icon">
+                <i className="fa fa-link"></i>
+              </div>
+            </a>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1 style={{fontSize:'30px'}}>Check Out Some of My Works.</h1>
+
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            {projects}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
